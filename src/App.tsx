158359547ae4c0cb992d4e11db2b337c7735f799
Fernanda Batalha de Mello <fernandam04@outlook.com>
2023-09-14 15:46:32 -0300

    initial settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import { Button } from './components/ui/button'
 
 
 export function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <h1>Hello Word</h1>
+    <Button>Teste</Button>
   )
 }
 
